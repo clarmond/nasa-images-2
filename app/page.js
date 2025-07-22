f98114e3441b8045f5cfb5f046fb8bf4b01589d0
Chad Armond <chad.l.armond@nasa.gov>
2025-07-22 08:46:11 -0500

    Fix for wonky title

--- app/page.js
+++ app/page.js
@@ -6,19 +6,21 @@ export default async function Home() {
   const resp = await fetch('https://images-assets.nasa.gov/popular.json');
   const results = await resp.json();
   const images = results.collection.items.map((item) => {
+    const title = item.data[0].title.replace('Apollo_11_moonwalk_montage_720p', 'Apollo 11 Moonwalk Montage');
     return {
       altText: item.data[0].description_508,
       description: item.data[0].description,
       href: `https://images.nasa.gov/details/${item.data[0].nasa_id}`,
       imageURL: item.links[0].href,
       nasaID: item.data[0].nasa_id,
-      title: item.data[0].title,
+      title,
     }
   });
   const filteredImages = images.slice(0, 24);
 
   return (
     <div>
+      <h1>Popular NASA Images</h1>
       <ImageGrid images={filteredImages} />
     </div>
   );
